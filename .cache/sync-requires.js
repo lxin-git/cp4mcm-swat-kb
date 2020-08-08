38018ba8c17218dea4ab2f3588dc8641188b8dfc
Mark Leftwich <LEFTWICH@uk.ibm.com>
2020-08-08 11:21:58 +0100

    changed document titles to fit Nav

---
+++ .cache/sync-requires.js
@@ -0,0 +1,42 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/Mark/Documents/GitHub/cp4mcm-swat-kb/.cache/dev-404-page.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("/Users/Mark/Documents/GitHub/cp4mcm-swat-kb/src/pages/404.js"))),
+  "component---src-pages-announcements-new-mdx": hot(preferDefault(require("/Users/Mark/Documents/GitHub/cp4mcm-swat-kb/src/pages/announcements/new.mdx"))),
+  "component---src-pages-blog-placeholder-placeholder-mdx": hot(preferDefault(require("/Users/Mark/Documents/GitHub/cp4mcm-swat-kb/src/pages/blog/placeholder/placeholder.mdx"))),
+  "component---src-pages-cp-4-a-guides-guide-1-index-mdx": hot(preferDefault(require("/Users/Mark/Documents/GitHub/cp4mcm-swat-kb/src/pages/cp4a-guides/guide1/index.mdx"))),
+  "component---src-pages-cp-4-i-guides-aspera-proxy-cp-4-i-index-mdx": hot(preferDefault(require("/Users/Mark/Documents/GitHub/cp4mcm-swat-kb/src/pages/cp4i-guides/aspera-proxy-cp4i/index.mdx"))),
+  "component---src-pages-cp-4-i-guides-aspera-quick-install-mvp-aspera-quick-install-mvp-mdx": hot(preferDefault(require("/Users/Mark/Documents/GitHub/cp4mcm-swat-kb/src/pages/cp4i-guides/aspera-quick-install-mvp/aspera-quick-install-mvp.mdx"))),
+  "component---src-pages-cp-4-i-guides-aspera-quick-install-mvp-client-mdx": hot(preferDefault(require("/Users/Mark/Documents/GitHub/cp4mcm-swat-kb/src/pages/cp4i-guides/aspera-quick-install-mvp/client.mdx"))),
+  "component---src-pages-cp-4-i-guides-aspera-quick-install-mvp-optional-mdx": hot(preferDefault(require("/Users/Mark/Documents/GitHub/cp4mcm-swat-kb/src/pages/cp4i-guides/aspera-quick-install-mvp/optional.mdx"))),
+  "component---src-pages-cp-4-i-guides-aspera-quick-install-mvp-server-mdx": hot(preferDefault(require("/Users/Mark/Documents/GitHub/cp4mcm-swat-kb/src/pages/cp4i-guides/aspera-quick-install-mvp/server.mdx"))),
+  "component---src-pages-cp-4-i-guides-tether-windows-index-mdx": hot(preferDefault(require("/Users/Mark/Documents/GitHub/cp4mcm-swat-kb/src/pages/cp4i-guides/tether-windows/index.mdx"))),
+  "component---src-pages-cp-4-i-guides-tether-windows-info-mdx": hot(preferDefault(require("/Users/Mark/Documents/GitHub/cp4mcm-swat-kb/src/pages/cp4i-guides/tether-windows/info.mdx"))),
+  "component---src-pages-general-guides-adding-video-adding-video-md": hot(preferDefault(require("/Users/Mark/Documents/GitHub/cp4mcm-swat-kb/src/pages/general-guides/adding-video/adding-video.md"))),
+  "component---src-pages-general-guides-creating-an-article-creating-new-entry-mdx": hot(preferDefault(require("/Users/Mark/Documents/GitHub/cp4mcm-swat-kb/src/pages/general-guides/creating-an-article/creating-new-entry.mdx"))),
+  "component---src-pages-general-guides-ssh-auth-multiple-github-accounts-ssh-auth-multiple-github-accounts-mdx": hot(preferDefault(require("/Users/Mark/Documents/GitHub/cp4mcm-swat-kb/src/pages/general-guides/ssh-auth-multiple-github-accounts/ssh-auth-multiple-github-accounts.mdx"))),
+  "component---src-pages-general-guides-wildcard-ssl-cert-lets-encrypt-wildcard-ssl-cert-lets-encrypt-mdx": hot(preferDefault(require("/Users/Mark/Documents/GitHub/cp4mcm-swat-kb/src/pages/general-guides/wildcard-ssl-cert-lets-encrypt/wildcard-ssl-cert-lets-encrypt.mdx"))),
+  "component---src-pages-ibm-cloud-private-icp-install-icp-predeploy-mdx": hot(preferDefault(require("/Users/Mark/Documents/GitHub/cp4mcm-swat-kb/src/pages/ibm-cloud-private/icp-install/icp-predeploy.mdx"))),
+  "component---src-pages-ibm-cloud-private-icp-install-info-mdx": hot(preferDefault(require("/Users/Mark/Documents/GitHub/cp4mcm-swat-kb/src/pages/ibm-cloud-private/icp-install/info.mdx"))),
+  "component---src-pages-ibm-cloud-private-icp-test-icptest-mdx": hot(preferDefault(require("/Users/Mark/Documents/GitHub/cp4mcm-swat-kb/src/pages/ibm-cloud-private/icp-test/icptest.mdx"))),
+  "component---src-pages-ibm-cloud-private-mark-test-scenario-mdx": hot(preferDefault(require("/Users/Mark/Documents/GitHub/cp4mcm-swat-kb/src/pages/ibm-cloud-private/mark-test/Scenario.mdx"))),
+  "component---src-pages-index-mdx": hot(preferDefault(require("/Users/Mark/Documents/GitHub/cp4mcm-swat-kb/src/pages/index.mdx"))),
+  "component---src-pages-itm-spufi-1-how-to-retrieve-live-information-spufi-md": hot(preferDefault(require("/Users/Mark/Documents/GitHub/cp4mcm-swat-kb/src/pages/ITM/spufi-1/how-to-retrieve-live-information-spufi.md"))),
+  "component---src-pages-itm-spufi-2-spufi-2-mdx": hot(preferDefault(require("/Users/Mark/Documents/GitHub/cp4mcm-swat-kb/src/pages/ITM/spufi-2/spufi2.mdx"))),
+  "component---src-pages-labs-about-aspera-about-aspera-mdx": hot(preferDefault(require("/Users/Mark/Documents/GitHub/cp4mcm-swat-kb/src/pages/labs/about-aspera/about-aspera.mdx"))),
+  "component---src-pages-labs-high-speed-file-transfer-at-the-speed-of-business-cli-mdx": hot(preferDefault(require("/Users/Mark/Documents/GitHub/cp4mcm-swat-kb/src/pages/labs/high-speed-file-transfer-at-the-speed-of-business/cli.mdx"))),
+  "component---src-pages-labs-high-speed-file-transfer-at-the-speed-of-business-client-and-cp-4-i-mdx": hot(preferDefault(require("/Users/Mark/Documents/GitHub/cp4mcm-swat-kb/src/pages/labs/high-speed-file-transfer-at-the-speed-of-business/client-and-cp4i.mdx"))),
+  "component---src-pages-labs-high-speed-file-transfer-at-the-speed-of-business-cloud-to-cloud-mdx": hot(preferDefault(require("/Users/Mark/Documents/GitHub/cp4mcm-swat-kb/src/pages/labs/high-speed-file-transfer-at-the-speed-of-business/cloud-to-cloud.mdx"))),
+  "component---src-pages-labs-high-speed-file-transfer-at-the-speed-of-business-connect-and-aoc-mdx": hot(preferDefault(require("/Users/Mark/Documents/GitHub/cp4mcm-swat-kb/src/pages/labs/high-speed-file-transfer-at-the-speed-of-business/connect-and-aoc.mdx"))),
+  "component---src-pages-labs-high-speed-file-transfer-at-the-speed-of-business-distribution-mdx": hot(preferDefault(require("/Users/Mark/Documents/GitHub/cp4mcm-swat-kb/src/pages/labs/high-speed-file-transfer-at-the-speed-of-business/distribution.mdx"))),
+  "component---src-pages-labs-high-speed-file-transfer-at-the-speed-of-business-node-api-mdx": hot(preferDefault(require("/Users/Mark/Documents/GitHub/cp4mcm-swat-kb/src/pages/labs/high-speed-file-transfer-at-the-speed-of-business/node-api.mdx"))),
+  "component---src-pages-labs-high-speed-file-transfer-at-the-speed-of-business-prerequisite-preparing-mdx": hot(preferDefault(require("/Users/Mark/Documents/GitHub/cp4mcm-swat-kb/src/pages/labs/high-speed-file-transfer-at-the-speed-of-business/prerequisite-preparing.mdx"))),
+  "component---src-pages-labs-high-speed-file-transfer-at-the-speed-of-business-prerequisite-troubleshooting-mdx": hot(preferDefault(require("/Users/Mark/Documents/GitHub/cp4mcm-swat-kb/src/pages/labs/high-speed-file-transfer-at-the-speed-of-business/prerequisite-troubleshooting.mdx"))),
+  "component---src-pages-labs-high-speed-file-transfer-at-the-speed-of-business-prerequisites-mdx": hot(preferDefault(require("/Users/Mark/Documents/GitHub/cp4mcm-swat-kb/src/pages/labs/high-speed-file-transfer-at-the-speed-of-business/prerequisites.mdx")))
+}
+
